f80fc9c481632039856baab6ae799f5f546fbcc9
padilhasam <jeferson.padilha86@gmail.com>
2020-09-23 23:24:27 -0300

    Teste em C

---
+++ teste.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(){
+	
+	
+	int a, b, soma;
+	
+	printf("Digite o valor de A: ");
+	scanf("%i", &a);
+	
+	printf("Digite o valor de B: ");
+	scanf("%i", &b);
+	
+		soma = a + b;
+	
+	printf("Resultado: %i", soma);
+	
+}
+
